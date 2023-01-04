df8fc0e596d0181f218c6955af47a21d744c08c5
landonenvoy <88791483+landonenvoy@users.noreply.github.com>
2023-01-04 13:34:10 -0700

    created table to multiple primes

--- app/prime_table.rb
+++ app/prime_table.rb
@@ -4,13 +4,11 @@ class PrimeTable
         rows = prime_numbers
         columns = prime_numbers
 
-        print "     "
         columns.each {|column_num| print " %-3d " % column_num}
         print "\n\n"
         rows.each do |row_num|
           print "%-3d| " % row_num
           columns.each {|column_num| print " %-3d " % (column_num * row_num)}
-          print "\n\n"
         end
 
     end
