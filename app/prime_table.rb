964162215bc78c6e7f4980f9ce9e63329f42562a
landonenvoy <88791483+landonenvoy@users.noreply.github.com>
2023-01-04 12:54:48 -0700

    created sieve of eratosthenes to generate list of primes

--- app/prime_table.rb
+++ app/prime_table.rb
@@ -1,9 +1,23 @@
 class PrimeTable
     def create_table(number)
-        prime_numbers = get_primes(number)
+        prime_numbers = get_primes()
+        p prime_numbers
     end
 
-    def get_primes(number)
-        
+    def get_primes()
+      primes = []
+      limit = 10000000
+      for i in 2 .. limit
+        primes[i] = i
+      end
+      for i in 2 .. Math.sqrt(limit)
+        next unless primes[i]
+          (i*i).step(limit, i) do |j|
+            primes[j] = nil
+        end
+      end
+      primes.compact.first(7)
     end
 end
+
+PrimeTable.new.create_table(7)
