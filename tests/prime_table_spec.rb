83c1290bafe81d5ba14921fdb1a9ee3dca102110
landonenvoy <88791483+landonenvoy@users.noreply.github.com>
2023-01-04 13:57:48 -0700

    added tests for get_primes function

--- tests/prime_table_spec.rb
+++ tests/prime_table_spec.rb
@@ -1,3 +1,24 @@
-require_relative 'spec_helper'
 require 'rspec'
-require_relative 'app/prime_table'
+require_relative '../app/prime_table'
+
+describe PrimeTable do
+
+    before do
+      @prime_table = PrimeTable.new
+    end
+  
+    describe "get_primes function" do
+  
+      context 'when user inputs the number 12' do
+        it 'primes array should have a length of 12' do
+          expect(@prime_table.get_primes(12).length == 12)
+        end
+      end
+  
+      context 'when user inputs the number 3' do
+        it 'primes array should have a length of 3' do
+          expect(@prime_table.get_primes(3).length == 3)
+        end
+      end
+    end
+  end
