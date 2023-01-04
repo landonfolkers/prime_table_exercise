4623cc3a01f2afbead5e4cbf9faebdde15e00aa7
landonenvoy <88791483+landonenvoy@users.noreply.github.com>
2023-01-04 13:34:45 -0700

    added formatting for table

--- app/prime_table.rb
+++ app/prime_table.rb
@@ -4,13 +4,14 @@ class PrimeTable
         rows = prime_numbers
         columns = prime_numbers
 
+        print "     "
         columns.each {|column_num| print " %-3d " % column_num}
         print "\n\n"
         rows.each do |row_num|
           print "%-3d| " % row_num
           columns.each {|column_num| print " %-3d " % (column_num * row_num)}
+          print "\n\n"
         end
-
     end
 
     def get_primes(number)
