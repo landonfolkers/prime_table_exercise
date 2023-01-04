ec832cce689759c93a4d19df81101ba070aa7e3a
landonenvoy <88791483+landonenvoy@users.noreply.github.com>
2023-01-04 13:33:41 -0700

    allowed class to get variable from console

--- app/prime_table.rb
+++ app/prime_table.rb
@@ -1,7 +1,17 @@
 class PrimeTable
     def create_table(number)
         prime_numbers = get_primes(number)
-        p prime_numbers
+        rows = prime_numbers
+        columns = prime_numbers
+
+        print "     "
+        columns.each {|column_num| print " %-3d " % column_num}
+        print "\n\n"
+        rows.each do |row_num|
+          print "%-3d| " % row_num
+          columns.each {|column_num| print " %-3d " % (column_num * row_num)}
+          print "\n\n"
+        end
 
     end
 
